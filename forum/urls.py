bc5b8a7acf7ebd1ea9069a92142390d69b456094
softwaredevPK <pskotarba@gmail.com>
2021-04-19 17:55:51 +0200

    Base template created

--- forum/urls.py
+++ forum/urls.py
@@ -1,4 +1,4 @@
-"""MyForum URL Configuration
+"""django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('forum.urls'))
+    path('', views.main_page, name='main-page')
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def main_page(request):
+     return render(request, 'forum/base_template.html')  # temporary, to see how it looks
+
+
+
