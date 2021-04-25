79d72f4b394ae6a044da26ebd4285fd64f591143
softwaredevPK <pskotarba@gmail.com>
2021-04-25 17:01:25 +0200

    Logout page created

--- users/urls.py
+++ users/urls.py
@@ -22,5 +22,6 @@ from .forms import LoginForm
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile')
 ]
